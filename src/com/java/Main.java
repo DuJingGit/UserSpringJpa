6e7fd40139d1bb3bed8c461ad493eb6cb96aa8cb
DuJingGit <3038852926@qq.com>
2019-05-14 15:45:37 +0800

    add

--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -1,7 +1,11 @@
 package com.java;
 
 public class Main {
+	
 	public static void main(String[] args) {
 		System.out.println(1111);
+	}
+    public void test() {
+		  System.out.println(1111);
 	}
 }
